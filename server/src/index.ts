b2308246155bfc39787b80279c5546425b33ac5f
Shishir Karkera <dn.shishirgk@gmail.com>
2026-01-07 09:18:00 +0530

    project setup

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connectDB } from './db';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server is running');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
